1798e5cabf7b921bb06f27cc1870afc9c894f890
RomjanHossain <romjanhossain726526@gmail.com>
2020-01-17 21:03:56 +0600

    another

--- work/views.py
+++ work/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Lists
 # Create your views here.
+
+
+def base(request):
+    return render(request, 'base.html')
+
+
+def home(request):
+    items = Lists.objects.all
+    context = {'lists': items}
+    return render(request, 'home.html', context)
